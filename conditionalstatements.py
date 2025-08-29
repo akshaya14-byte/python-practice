80e0f960e16a4f6b8f948aba3112631c8fcc1aa2
akshay <24f3000496@ds.study.iitm.ac.in>
2025-08-29 15:44:04 +0530

    updated

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -1,7 +1,15 @@
 #checking voting eligibility
 age = int(input("Enter your age: "))
-if age >= 18:
-    print("You are eligible to vote.")
-else:
-    print("You are not eligible to vote.")
+if age < 0 or age > 100:
+    print("Invalid age.")
+
+elif age < 18:
     print("You can register to vote once you turn 18.")
+
+elif age >= 18:
+    print("You are eligible to vote.")
+else: 
+    print("You are eligible to vote.")
+    # git add .
+    # git commit -m "Your commit message"
+    # git push origin main
