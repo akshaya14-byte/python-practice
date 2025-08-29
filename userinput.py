ead218ac624f709938e43a590bbaf9f9fb6ab969
akshay <24f3000496@ds.study.iitm.ac.in>
2025-08-29 12:29:30 +0530

    Added madlibs.py and updated useriput.py

--- userinput.py
+++ userinput.py
@@ -1,22 +1,23 @@
 #shopping cart program
+cart = []
+
 while True:
     print("Welcome to the shopping cart program!")
     print("You can add items to your cart.")
     item = input("Enter the item you want to add to the cart: " )
-    quantity = input("Enter the quantity: ")
-    price = input("Enter the price: ")
-    cart = []
+    quantity = int(input("Enter the quantity: "))
+    price = float(input("Enter the price: "))
+
     cart.append({"item": item, "quantity": quantity, "price": price})
     choice = input("Do you want to add more items to the cart? (yes/no)").lower()
-    if choice == "yes":
-        item = input("Enter the item you want to add to the cart: " )
-        quantity = input("Enter the quantity: ")
-        price = input("Enter the price: ")
-        cart.append({"item": item, "quantity": quantity, "price": price})
-        if choice == "no":
+    if choice == "no":
             break
-    else:
-        print("No more items to add.")
+    
 print("Your shopping cart contains:")
+subtotal = [i["quantity"] * i["price"] for i in cart]
 for i in cart:
-    print(f"Item: {i['item']}, Quantity: {i['quantity']}, Price: {i['price']}")
+    print(f"Item: {i['item']}, Quantity: {i['quantity']}, Price: ${i['price']}")
+    print(f"Subtotal: ${i['quantity'] * i['price']}")
+total = sum([i["quantity"] * i["price"] for i in cart])
+print(f"Total: ${total}")
+print("Thank you for shopping with us!")
