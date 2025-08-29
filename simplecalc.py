730c7ac150ac1b2384d9d948c3c62a3a3e1fcc27
akshay <24f3000496@ds.study.iitm.ac.in>
2025-08-29 18:18:14 +0530

    calculator basic

---
+++ simplecalc.py
@@ -0,0 +1,19 @@
+#python calculator
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Division by zero error"
+else:
+    result = "Invalid operator"
+print(result)
