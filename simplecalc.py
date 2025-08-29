05c198b6c4119a43689eed07b8b882d329120d21
akshay <24f3000496@ds.study.iitm.ac.in>
2025-08-29 18:25:58 +0530

    more features

--- simplecalc.py
+++ simplecalc.py
@@ -1,7 +1,8 @@
 #python calculator
+import math 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-operator = input("Enter operator (+, -, *, /): ")
+operator = input("Enter operator (+, -, *, /, **, sqrt): ")
 
 if operator == '+':
     result = num1 + num2
@@ -9,6 +10,10 @@ elif operator == '-':
     result = num1 - num2
 elif operator == '*':
     result = num1 * num2
+elif operator == '**':
+    result = num1 ** num2
+elif operator == 'sqrt':
+    result = math.sqrt(num1 * num2)
 elif operator == '/':
     if num2 != 0:
         result = num1 / num2
