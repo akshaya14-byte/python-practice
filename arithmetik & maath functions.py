db1ed4f4219a0b543ce4b3083bc25ad43b1af9b8
akshay <24f3000496@ds.study.iitm.ac.in>
2025-08-29 12:57:12 +0530

    arithmetik&maath functions.py

---
+++ arithmetik & maath functions.py
@@ -0,0 +1,26 @@
+import math
+# value = 10
+# value += 1
+# value -= 1
+# value *= 2
+# value /= 2
+# value %=2
+# print(value)
+#result = round(x)
+#result = abs(y)
+#result = pow(z, 3)
+#result = max(x, y, z)
+#result = min(x, y, z)
+#print(result) 
+
+
+#circumference of a circle
+# radius = int(input("Enter the radius of the circle: "))
+# circumference = 2 * math.pi * radius
+# print("Circumference of the circle:", round(circumference))
+
+#area of the circle
+
+# radius = int(input("Enter the radius of the circle: "))
+# area = math.pi * radius ** 2
+# print("Area of the circle:", round(area))
