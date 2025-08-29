3db8f498b3b11019c91bf12c83238e80e5fc34f5
akshay <24f3000496@ds.study.iitm.ac.in>
2025-08-29 18:29:14 +0530

    updated

--- simplecalc.py
+++ simplecalc.py
@@ -13,7 +13,11 @@ elif operator == '*':
 elif operator == '**':
     result = num1 ** num2
 elif operator == 'sqrt':
-    result = math.sqrt(num1 * num2)
+    choice = input("Calculate sqrt of which number? (1/2): ")
+    if choice == '1':
+        result = math.sqrt(num1)
+    elif choice == '2':
+        result = math.sqrt(num2)
 elif operator == '/':
     if num2 != 0:
         result = num1 / num2
