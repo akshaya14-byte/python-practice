21ac348b5555470a7e41b9a5e3704946609cf0fb
akshay <24f3000496@ds.study.iitm.ac.in>
2025-09-04 22:46:18 +0530

    strings

--- strings.py
+++ strings.py
@@ -1,6 +1,13 @@
-name =  input("Enter your name: ")
+#name =  input("Enter your name: ")
 
 #result = len(name)
 #result = name.rfind("a")
-result = name.isdigit()
-print(result)
+#result = name.isdigit()
+#result = name.replace("a", "A")
+#print(result)
+
+#indexing
+
+numbers = "0123456789"
+#print(numbers[0:5])
+
