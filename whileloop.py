ba767b0b0477208c721957868d127065e3b784f5
A K <akshayavardhan20061418@gmail.com>
2025-09-05 22:22:24 +0530

    while loop

---
+++ whileloop.py
@@ -0,0 +1,23 @@
+#numbers from 1 to 20
+# Initialize i to 2
+# While i is less than or equal to 20 and i is even
+#     Print the value of i
+#     Increment i by 2
+#n = int(input("Enter a number: "))
+#i = 1
+#total = 0
+#while i <= n:
+     #total += i
+     #i += 1
+#print(total)
+
+#factorial of a number
+#i = 1
+#actorial = 1
+#hile i <= n:
+   #actorial *= i
+  #i += 1
+#rint(factorial)
+
+
+    
