f14c7bcb5756a4efc2afbeb6211d78e868c33f7e
A K <akshayavardhan20061418@gmail.com>
2025-09-06 16:21:24 +0530

    while loop problems

--- whileloop.py
+++ whileloop.py
@@ -1,23 +1,38 @@
-#numbers from 1 to 20
-# Initialize i to 2
-# While i is less than or equal to 20 and i is even
-#     Print the value of i
-#     Increment i by 2
-#n = int(input("Enter a number: "))
-#i = 1
-#total = 0
-#while i <= n:
-     #total += i
-     #i += 1
-#print(total)
-
-#factorial of a number
-#i = 1
-#actorial = 1
-#hile i <= n:
-   #actorial *= i
-  #i += 1
-#rint(factorial)
 
 
-    
+# i = 1
+# while i <= numbers:
+#     print(i)
+#     i += 1
+#num = int(input("Enter a number: "))
+# i = 2
+# total = 0
+#
+# while i <= num and i % 2 == 0:
+#     digit = num % 10
+#     total += digit * i
+#     num //= 10
+#     i += 2
+# num = int(input("Enter a number: "))
+
+
+# palindrome = 0
+# original_num = num
+# while num > 0:
+#     last_digit = num % 10
+#     palindrome = palindrome * 10 + last_digit
+#     num //= 10
+# if original_num == palindrome:
+#     print("The number is a palindrome")
+# else:
+#     print("The number is not a palindrome")
+
+
+# num = int(input("Enter a number: "))
+#
+# i = 1
+# count = 0
+# while num > 0:
+#     num //= 10
+#     count += 1
+# print(count)
